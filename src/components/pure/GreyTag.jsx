952bcce37ad65695404d0fbd01eff3266a37a595
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2022-01-19 19:13:49 +0100

    StudentTags done

--- src/components/pure/GreyTag.jsx
+++ src/components/pure/GreyTag.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function GreyTag({ tag }) {
+function GreyTag({ tag, tags }) {
+
+    const deleteTag = () => {
+        
+    }
+
     return (
         <div>
-            <span className="tagitem" id="1">{ tag.name }<svg  xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+            <span className="tagitem" id="1">{ tag.name }<svg  onClick={ deleteTag } xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
             </span>
@@ -13,7 +18,8 @@ function GreyTag({ tag }) {
 }
 
 GreyTag.propTypes = {
-    tag: PropTypes.object.isRequired
+    tag: PropTypes.object.isRequired,
+    tags: PropTypes.array.isRequired
 };
 
 export default GreyTag;
